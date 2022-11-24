070890ec91e37952a313b1961e6cdfa075b02a20
Calvin Ham <calvinjunham@gmail.com>
2022-11-23 21:51:57 -0700

    fix: update from web-playground -> app

--- apps/app-e2e/src/e2e/app.cy.ts
+++ apps/app-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('web-playground', () => {
+describe('app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('web-playground', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome web-playground');
+    getGreeting().contains('Welcome app');
   });
 });

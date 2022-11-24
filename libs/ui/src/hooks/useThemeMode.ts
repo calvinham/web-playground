f0eaf1e09f8c958347d9f81ce403fe7d1e36118a
Calvin Ham <calvinjunham@gmail.com>
2022-11-23 20:13:46 -0700

    add generated + fix tsconfig

--- libs/ui/src/hooks/useThemeMode.ts
+++ libs/ui/src/hooks/useThemeMode.ts
@@ -2,13 +2,15 @@ import { useCallback, useEffect } from 'react';
 import { useAtom } from 'jotai';
 import { ThemeMode, themeModeAtom } from '../state';
 
+const THEME_MODE = 'themeMode';
+
 export default function useThemeMode(): readonly [ThemeMode, () => void] {
   const [theme, setTheme] = useAtom(themeModeAtom);
 
   useEffect(() => {
-    const _mode = localStorage.getItem('themeMode');
+    const _mode = localStorage.getItem(THEME_MODE);
     if (!_mode) {
-      localStorage.setItem('themeMode', theme);
+      localStorage.setItem(THEME_MODE, theme);
     }
   }, [theme]);
 
